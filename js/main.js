86b8795b106d28b6625b77c122e4fa8980c82f3d
calin112 <40755926+calin112@users.noreply.github.com>
2019-05-09 15:23:30 +0200

    Added video, done links and loading screen

--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
 //Smooth scrolling
 
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-                || location.hostname == this.hostname) {
-        var hashStr = this.hash.slice(1);
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + hashStr +']');
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-        if (target.length) {
-            $('html,body').animate({ scrollTop: target.offset().top}, 1000);
-            window.location.hash = hashStr;
-            return false;
-        }
-    }
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
+
+//Navbar Collapse
+
+$(document).ready(function(){
+  $('.nav-hide').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+});
+
+//Preloader
+
+$(window).on('load',function(){
+  $('.preloader').addClass('complete')
+})
+
+//Loading icon
+
+$(window).on('load',function(){
+  $('.loader').addClass('complete')
+})
+
+var vid = document.getElementById("myVideo");
+vid.playbackRate = 0.8;
